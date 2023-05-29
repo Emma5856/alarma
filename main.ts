487946ca02d89b9832f8d177b308049cf5593e8c
Emma5856 <126062132+Emma5856@users.noreply.github.com>
2023-05-29 09:46:25 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,35 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.magneticForce(Dimension.Strength))
+radio.onReceivedNumber(function (receivedNumber) {
+    EstadoAlarma = receivedNumber
 })
+let EstadoAlarma = 0
+radio.setGroup(123)
 basic.forever(function () {
+    radio.sendNumber(EstadoAlarma)
     if (input.magneticForce(Dimension.Strength) < 200) {
-        basic.showIcon(IconNames.Angry)
+        EstadoAlarma = 1
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
-    } else {
+        if (EstadoAlarma == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
+        }
+    } else if (input.magneticForce(Dimension.X) >= 199) {
+        EstadoAlarma = 0
         basic.clearScreen()
+        if (EstadoAlarma == 3) {
+            basic.clearScreen()
+        }
     }
 })
